1958bdb3c008135f368afa6b04f51c284f0da138
ecl1pse <hlustalex@gmail.com>
2025-11-11 21:38:33 +0300

    Update ConsoleApplication3.cpp

--- ConsoleApplication3.cpp
+++ ConsoleApplication3.cpp
@@ -1,4 +1,45 @@
 // ConsoleApplication4.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//№1
+#include <stdio.h>
+#include <math.h>
+double mySin(double x, double eps) {
+    double cn = x;
+    double s = cn;
+    for (int n = 3; fabs(cn) > eps; n += 2) {
+        cn = -cn * x * x / (n * (n - 1));
+        s += cn;
+    }
+    return s;
+}
+
+int main()
+{
+    double x_start = 0.0;
+    double x_end = 2.0;
+    double dx = 0.2;
+    double eps = 0.0001;
+    for (double x = x_start; x <= x_end; x += dx) {
+        double s = mySin(x, eps);
+        printf("%f %f\n", x, s);
+    }
+    printf("\n%lf %lf", mySin(3.141592 / 3, 0.0000001), sqrt(3) / (2));
+    return 0;
+}
+
+//№2
+#include <stdio.h>
+int third(int num) {
+    int n = num;
+    n = n / 100;
+    return n - (n / 10) * 10;
+}
+int main() 
+{
+    int num;
+    scanf_s("%d", &num);
+    int result = third(num);
+    printf("%d\n", result);
+}
 //№3
 
 #include <stdio.h>
